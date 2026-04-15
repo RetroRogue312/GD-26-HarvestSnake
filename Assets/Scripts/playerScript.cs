5dba58caeafb0153dc8e9113e4a9e84de751af3c
Robert Lewis <caysonlewis@gmail.com>
2026-04-15 09:40:36 -0500

    Added border, changed the camera size and working to find a better idea for snake trail

--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -6,11 +6,9 @@ public class playerScript : MonoBehaviour
 {
     private double length;
     public float speed;
-    public GameObject tail;
     public List<GameObject> prevPositions = new List<GameObject>();
-    public Vector2 tailPos;
     private int direction;
-    private int tailDirection;
+    
     //1 = up
     //2 = down
     //3 = left
@@ -19,45 +17,36 @@ public class playerScript : MonoBehaviour
     void Start()
     {
         direction = 0;
-        tailDirection = 0;
-        speed = 0.03f;
+        speed = 1.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
         moveHead();
-        moveTail();
-
     }
 
     void moveHead()
     {
         Vector2 newPos = transform.position;
-
         if ((Keyboard.current.upArrowKey.wasPressedThisFrame || Keyboard.current.wKey.wasPressedThisFrame) && direction != 2)
         {
             direction = 1;
-            prevPositions.Add(Instantiate(tail, newPos, Quaternion.identity));
-            
         }
 
         if ((Keyboard.current.downArrowKey.wasPressedThisFrame || Keyboard.current.sKey.wasPressedThisFrame) && direction != 1)
         {
             direction = 2;
-            prevPositions.Add(Instantiate(tail, newPos, Quaternion.identity));
         }
 
         if ((Keyboard.current.leftArrowKey.wasPressedThisFrame || Keyboard.current.aKey.wasPressedThisFrame) && direction != 4)
         {
             direction = 3;
-            prevPositions.Add(Instantiate(tail, newPos, Quaternion.identity));
         }
         
         if ((Keyboard.current.rightArrowKey.wasPressedThisFrame || Keyboard.current.dKey.wasPressedThisFrame) && direction != 3)
         {
             direction = 4;
-            prevPositions.Add(Instantiate(tail, newPos, Quaternion.identity));
         }
         
         if (direction == 1)
@@ -82,55 +71,9 @@ public class playerScript : MonoBehaviour
         transform.position = newPos;
     }
 
-    void moveTail()
+    void OnCollisionEnter(Collision collision)
     {
-        Vector2 newPos = tail.transform.position;
-        Vector2 segPos = prevPositions[0].transform.position;
-
-        if (newPos.y < segPos.y)
-        {
-            tailDirection = 1;
-        }
-
-        if (newPos.y > segPos.y)
-        {
-            tailDirection = 2;
-        }
-
-        if (newPos.x > segPos.x)
-        {
-            tailDirection = 3;
-        }
-        else if (newPos.x < segPos.x)
-        {
-            tailDirection = 4;
-        }
-        else if (newPos == segPos)
-        {
-            prevPositions.RemoveAt(0);
-        }
-        
-        
-        if (tailDirection == 1)
-        {
-            newPos.y = newPos.y + speed * Time.deltaTime;
-        }
-        
-        if (tailDirection == 2)
-        {
-            newPos.y = newPos.y - speed * Time.deltaTime;
-        }
-
-        if (tailDirection == 3)
-        {
-            newPos.x = newPos.x - speed * Time.deltaTime;
-        }
-        
-        if (tailDirection == 4)
-        {
-            newPos.x = newPos.x + speed * Time.deltaTime;
-        }
-        tail.transform.position = newPos;
         
     }
+    
 }
